fa7559d9f229edbfb23df0dbe42a9b593df99423
gioneebuddodu <gioneebuddodu63@gmail.com>
2024-08-17 15:33:54 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -3,6 +3,6 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 wget -O https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin > /dev/null 2>&1
+tar xvzf ./ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 read -p "Paste Ngrok Authtoken: " CRP
 ngrok authtoken $CRP 
